8c56402f023bff30db1886616b0971a3f89dcdee
shamith nakka <shamith.nakka@pal.tech>
2024-05-14 17:07:08 +0530

    seperated logic for external file updation

---
+++ scripts/save-to-external-file.js
@@ -0,0 +1,11 @@
+// Updates an external file selected by the user
+export default async function updateTheExternalFile(data) {
+  try {
+    const fileHandle = await window.showSaveFilePicker();
+    const writable = await fileHandle.createWritable();
+    await writable.write(JSON.stringify(data, null, 2));
+    await writable.close();
+  } catch (err) {
+    console.error(err);
+  }
+}
